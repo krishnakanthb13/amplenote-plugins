22a97288c35d2d0a6fd8fa7cebb1d5013acb5ea3
Krishna Kanth B <75969083+krishnakanthb13@users.noreply.github.com>
2024-07-23 12:42:29 +0530

    Clean code
    w/o console.logs

--- extracttoanote2.0/extracttoanote2.js
+++ extracttoanote2.0/extracttoanote2.js
@@ -2,7 +2,7 @@
     replaceText: async function (app, text) {
         // Get the selected content from the app's context
         const textWithFormatting = app.context.selectionContent;
-        console.log("Selected Content:", textWithFormatting);
+        // console.log("Selected Content:", textWithFormatting);
 
         // Prompt the user to select a note to transfer the selected text
         const result = await app.prompt("Extract your selection to a New Note! Formatted!", {
@@ -17,17 +17,17 @@
         
         // Get the note name selected by the user
         const noteName = result;
-        console.log("Selected note name by the user:", noteName);
+        // console.log("Selected note name by the user:", noteName);
 
         // Prepare the formatted text to be pasted in the new note
         const hLine = `---`;
         const fDate = new Date();
         const textFinal = `${hLine}\n${fDate}\n${textWithFormatting}\n${hLine}`;
-        console.log("To be Pasted Content:", textFinal);
+        // console.log("To be Pasted Content:", textFinal);
 
         // Create a placeholder link to the destination note
         const destNoteloc = `[${noteName.name}](https://www.amplenote.com/notes/${noteName.uuid})`;
-        console.log("Placeholder Link to Destination Note:", destNoteloc);
+        // console.log("Placeholder Link to Destination Note:", destNoteloc);
 
         // Insert the formatted text into the selected note
         await app.insertNoteContent({
