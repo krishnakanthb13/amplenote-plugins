f71e3844cff9937f7cd21e3856219b69d91f4c07
Krishna Kanth B <75969083+krishnakanthb13@users.noreply.github.com>
2024-08-05 21:33:26 +0530

    Update notesreviewer.js
    All good, Commenting Pending

--- notesreviewer/notesreviewer.js
+++ notesreviewer/notesreviewer.js
@@ -8,9 +8,9 @@
                 label: "Select The Review Decision!",
                 type: "select",
                 options: [
-                    { label: "Keep - Review Completed!", value: "2-keep" },
-                    { label: "Discard - Review Completed!", value: "2-discard" },
-                    { label: "Review - Review Pending!", value: "2-review" }
+                    { label: "Keep - Review Completed!", value: "-notes-reviewer/2-keep" },
+                    { label: "Discard - Review Completed!", value: "-notes-reviewer/2-discard" },
+                    { label: "Review - Review Pending!", value: "-notes-reviewer/2-review" }
                 ]
             },
             { 
@@ -31,23 +31,36 @@
         app.alert("Operation has been cancelled. Tata! Bye Bye! Cya!");
         return;
     }
-
+    
     // ------- Log user selections for debugging -------
     console.log("Users Selection:", result);
 
+    // ------- The String UUID of the note the plugin action was invoked from. -------
+    const reportNoteUUID = app.context.noteUUID;
+    console.log("app.context.noteUUID:", app.context.noteUUID);
+    console.log("reportNoteUUID:", reportNoteUUID);
+    
     // ------- Predefined review tags -------
     const reviewTagz = '-notes-reviewer,-notes-reviewer/0-reports,-notes-reviewer/1-inbox,-notes-reviewer/2-discard,-notes-reviewer/2-keep,-notes-reviewer/2-review,-notes-reviewer/3-moved';
     console.log("reviewTagz:", reviewTagz);
 
     // ------- Destructuring user inputs -------
-    const [decisiontagName, multiTag, singleTag] = result;
-    console.log("decisiontagName:", decisiontagName);
+    const [decisionTagName, multiTag, singleTag] = result;
+    console.log("decisionTagName:", decisionTagName);
     console.log("multiTag:", multiTag);
     console.log("singleTag:", singleTag);
 
+    // ------- Check if the user cancelled the operation -------
+    if ((multiTag || singleTag) && decisionTagName) {
+      app.alert("Review Decision should not be selected when Multiple Tags or Single Tag are Used or Selected!");
+      return;
+    }
+    
     // ------- Get the link from the Link -------
     let decideUUID = (link.href || "");
+    // let noteDescription = (link.description || "");
     console.log("decideUUID:", decideUUID);
+    // console.log("noteDescription:",noteDescription);
 
     // ------- Create a regular expression to match UUID patterns -------
     const uuidRegex = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i;
@@ -65,19 +78,23 @@
         console.log("No UUID found in the URL.");
     }
 
+    let fTags = new Set();
+    // let fTagsR = new Set();
+    
     // ------- Remove predefined tags from the note -------
-    if (decisiontagName) {
+    if (decisionTagName) {
         const tagsArray = reviewTagz.split(',').map(tag => tag.trim());
         for (const tag of tagsArray) {
             if (tag) {
                 await app.removeNoteTag({ uuid: decideUUID }, tag);
+                // fTagsR.add(tag);
             }
         }
     }
 
     // ------- Add the selected decision tag to the note -------
-    const added = await app.addNoteTag({ uuid: decideUUID }, `${decisiontagName}`);
-    await app.alert(added ? "Tag added" : "Failed to add tag");
+    const added = await app.addNoteTag({ uuid: decideUUID }, `${decisionTagName}`);
+    fTags.add(decisionTagName);
 
     // ------- Add multiple tags to the note if provided -------
     if (multiTag) {
@@ -85,16 +102,57 @@
         for (const tag of tagsArray) {
             if (tag) {
                 await app.addNoteTag({ uuid: decideUUID }, tag);
+                fTags.add(tag);
             }
         }
         await app.addNoteTag({ uuid: decideUUID }, "-notes-reviewer/3-moved");
+        fTags.add("-notes-reviewer/3-moved");
     }
 
     // ------- Add single tag to the note if provided -------
     if (singleTag) {
         await app.addNoteTag({ uuid: decideUUID }, singleTag);
+        fTags.add(singleTag);
         await app.addNoteTag({ uuid: decideUUID }, "-notes-reviewer/3-moved");
+        fTags.add("-notes-reviewer/3-moved");
+        
     }
+
+    // ------- Audit Reporting -------
+    // Prepare the formatted text to be pasted in the new note
+    const fDate = new Date();
+    // Format the date using toLocaleString with options
+    const formattedDate = fDate.toLocaleString('en-US', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    });
+    console.log("fDate:",fDate);
+    console.log("formattedDate:",formattedDate);
+
+    const noteHandle = await app.findNote({ uuid: decideUUID });
+    const noteDescription = noteHandle.name;
+    const noteName = `[${noteDescription}](https://www.amplenote.com/notes/${decideUUID})`;
+    console.log("noteName:",noteName);
+    console.log("fTags:",fTags);
+    
+    const fTagsArray = Array.from(fTags);
+    console.log("fTagsArray:",fTagsArray);
+    // const fTag = `${decisiontagName},${singleTag},${multiTag}`
+    
+    const textFinal = `
+> Audit - Review Notes: On <mark data-text-color="25" style="color: #F8D616;">**${formattedDate}**</mark>, The Note Titled: **${noteName}** underwent a review decision process. The tag: <mark data-text-color="25" style="color: #F8D616;">[**${fTagsArray}**]</mark> was/were added.
+`;
+    console.log("textFinal:",textFinal);
+    
+    // Insert the formatted text into the selected note
+        await app.insertNoteContent({
+            uuid: reportNoteUUID
+        }, textFinal);
+    
   }
 
 }
