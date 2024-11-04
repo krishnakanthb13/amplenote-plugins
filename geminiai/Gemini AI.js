7adab87c73df6cd25ef8c95e2239faa3f1b9345d
Krishna Kanth B <75969083+krishnakanthb13@users.noreply.github.com>
2024-11-04 18:35:47 +0530

    Update Gemini AI.js
    Testing Multiple Notes

--- geminiai/Gemini AI.js
+++ geminiai/Gemini AI.js
@@ -382,4 +382,148 @@ imageOption: {
 },
 },
 //---------------------------
+// Function for handling options on a specific image by url
+//---------------------------
+appOption: {
+  "Multiple Notes": async function (app, image) {
+
+  const result1 = await app.prompt("Select the Tags, to choose the notes from. Disclaimer: Please be aware that humans may review or read any shared content to ensure compliance, quality, and accuracy in accordance with Gemini's policies.", {
+    inputs: [
+		  { label: "Select the Tags to see the notes From.", type: "tags", limit: 5 }
+    ]
+  });
+
+  // If the result is falsy, the user canceled the operation
+  if (!result1) {
+    app.alert("Operation has been cancelled. Tata! Bye Bye! Cya!");
+    return;
+  }
+
+	const noteHandles = await app.filterNotes({ tag: result1 });
+
+  const result2 = await app.prompt("Select the notes (Multiple-Notes allowed). Disclaimer: Please be aware that humans may review or read any shared content to ensure compliance, quality, and accuracy in accordance with Gemini's policies.", {
+    inputs: [
+		  { label: `${noteHandles.name} | ${noteHandles.tag}`, type: "checkbox", value: noteHandles.uuid },
+    ]
+  });
+
+  // If the result is falsy, the user canceled the operation
+  if (!result2) {
+    app.alert("Operation has been cancelled. Tata! Bye Bye! Cya!");
+    return;
+  }
+
+  // Prompt the user for desired actions with the note content
+  const result = await app.prompt("What do you want to do with this Notes. Disclaimer: Please be aware that humans may review or read any shared content to ensure compliance, quality, and accuracy in accordance with Gemini's policies.", {
+    inputs: [
+      {
+        label: "Gemini Model variants", 
+        type: "select", 
+        options: [
+          { label: "Gemini 1.5 Pro", value: "gemini-1.5-pro" }, 
+		  { label: "Gemini 1.0 Pro", value: "gemini-1.0-pro" }
+        ],
+        value: "gemini-1.5-pro"
+      },
+      { 
+        label: "Prompting Type", 
+        type: "select", 
+        options: [
+          { label: "Summarize", value: "Summarize these Multiple Notes" },
+          { label: "Actionable Points", value: "Convert into Actionable Points from these Multiple Notes" },
+          { label: "Other (Fill following boxes)", value: "Customized - Use the below Details" }
+        ],
+        value: "Summarize these Multiple Notes"
+      },
+      // Additional user inputs for customization of the AI response
+	  { label: "AI System Instructions", placeholder: "Eg: You are a cat. Your name is Neko. OR You are a Teacher, Review my Document.", type: "text" },
+      { label: "Add contextual information", placeholder: "Eg: Assume the audience is a group of high school students.", type: "string" },
+      { label: "Specify any constraints", placeholder: "Eg: Summarize in no more than 50 words.", type: "string" },
+      { label: "Define the format of the response", placeholder: "Eg: Provide a list of bullet points.", type: "string" },
+      { label: "Specify the tone or style", placeholder: "Eg: Formal, friendly, persuasive", type: "string" },
+      { label: "Other Free Text requests", placeholder: "Eg: Provide relevant examples or comparisons", type: "string" },
+    ]
+  });
+
+  // If the result is falsy, the user canceled the operation
+  if (!result) {
+    app.alert("Operation has been cancelled. Tata! Bye Bye! Cya!");
+    return;
+  }
+
+  // Extract user-selected inputs
+  const [modelVariant, promptSelect, systemInstruction, promptContext, promptConstraint, promptFormat, promptTone, promptOther] = result;
+  console.log("result",result);
+  const modelVariantz = modelVariant;
+  console.log("modelVariantz",modelVariantz);
+  let finalAIResponse;
+
+  // Fetch and clean markdown content from the note
+  const markdown = await app.getNoteContent({ uuid: noteUUID });
+  const cleanedMarkdown = markdown
+    .replace(/<!--[\s\S]*?-->/g, "")   // Remove HTML comments
+    .replace(/<mark[^>]*>/g, "")       // Remove opening <mark> tags with any attributes
+    .replace(/<\/mark>/g, "")          // Remove closing </mark> tags
+
+  //---------------------------
+  // Load external library for AI response
+  //---------------------------
+  function _loadLibrary(url) {
+    return import(url);
+  }
+
+  _loadLibrary("https://esm.run/@google/generative-ai").then(async ({ GoogleGenerativeAI }) => {
+    const API_KEY = app.settings["Gemini API Key"];
+    const genAI = new GoogleGenerativeAI(API_KEY);
+
+    const aiModel = genAI.getGenerativeModel({ model: `${modelVariantz}`, systemInstruction: `${systemInstruction}` });
+	console.log("aiModel",aiModel);
+
+    const promptAI = `${promptSelect}.\nContext:${promptContext || "None"}.\nConstraint:${promptConstraint || "None"}.\nFormat:${promptFormat || "Markdown"}.\nTone:${promptTone || "None"}.\nAdditional_Details:${promptOther || "None"}.\nText:${cleanedMarkdown}`;
+	console.log("promptAI",promptAI);
+
+    const aiResponse = await aiModel.generateContent(promptAI);
+	console.log("aiResponse",aiResponse);
+    finalAIResponse = aiResponse.response.text();
+	console.log("finalAIResponse",finalAIResponse);
+
+    //---------------------------
+    // Present the generated AI response to the user with further options
+	// Prompt user for response handling options (Copy or Replace Note Content)
+    //---------------------------
+    const result2 = await app.alert(`Gemini AI Response: ${finalAIResponse}`, {
+      actions: [     
+        { label: "Copy", value: "copytxt" },
+        { label: "New Note", value: "newnote" },
+      ]
+    });
+
+    if (!result2) { return; }
+    const actionResult = result2;
+
+    // Define a unique filename for the new note, if that option is selected
+    const now = new Date();
+    const YYMMDD = now.toISOString().slice(2, 10).replace(/-/g, '');
+    const HHMMSS = now.toTimeString().slice(0, 8).replace(/:/g, '');
+    const filename = `Gemini_AI_${YYMMDD}_${HHMMSS}`;
+
+    //---------------------------
+    // Handle user action for AI response (Copy to Clipboard or Create New Note)
+    //---------------------------
+    if (actionResult === "copytxt") {
+      await app.writeClipboardData(finalAIResponse);
+	  console.log("Copied to clipboard.");
+    } else if (actionResult === "newnote") {
+      const noteUUIDNew = await app.createNote(`${filename}`, [ "-reports/-gemini-ai" ]);
+	  console.log("New note Created.");
+      await app.insertContent({ uuid: noteUUIDNew }, finalAIResponse);
+      await app.navigate(`https://www.amplenote.com/notes/${noteUUIDNew}`);
+    }
+
+  }).catch(error => {
+    console.error("Failed to load library or execute code:", error);
+  });
+},
+},
+//---------------------------
 }
