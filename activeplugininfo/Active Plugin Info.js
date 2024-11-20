22c7700ac263334dce8f8776c4a8df70fbae7540
Krishna Kanth B <75969083+krishnakanthb13@users.noreply.github.com>
2024-11-20 18:00:03 +0530

    Update Active Plugin Info.js
    console log commented

--- activeplugininfo/Active Plugin Info.js
+++ activeplugininfo/Active Plugin Info.js
@@ -32,7 +32,7 @@
 	let notesG = await app.filterNotes({ group: notesGroup });
 	// Sort notes by their name in ascending order
 	notesG.sort((a, b) => a.name.localeCompare(b.name));
-	console.log("Sorted notes by name:", notesG);
+	// console.log("Sorted notes by name:", notesG);
 	for (const noteHandleG of notesG) {
 		notesGroupNames.add(`- [${noteHandleG.name || "Untitled Note"}](https://www.amplenote.com/notes/${noteHandleG.uuid})`);
 		const markdown = await app.getNoteContent({ uuid: noteHandleG.uuid });
@@ -41,15 +41,15 @@
 		const formattedFoundWords = foundWords.map(word => ` - ${word}`).join("\n");
 		pluginInfo += `### ${noteHandleG.name}<!-- {"collapsed":true} -->\n`;
 		pluginInfo += `${formattedFoundWords}\n\n`;
-		console.log("Plugin Name:", noteHandleG.name);
-		console.log("Found words:", foundWords);
-		console.log("Formatted search words:\n", formattedFoundWords);
+		// console.log("Plugin Name:", noteHandleG.name);
+		// console.log("Found words:", foundWords);
+		// console.log("Formatted search words:\n", formattedFoundWords);
 	}
     pluginInfo += `\n---`;
 	results = new Set(notesGroupNames);
 	// console.log("Sorted notesG:", notesG);
 	// console.log("Sorted notesGroupNames:", notesGroupNames);
-	console.log("pluginInfo\n", pluginInfo);
+	// console.log("pluginInfo\n", pluginInfo);
 	results = Array.from(results);
 	const resultText = results.join("\n");
 	// Generate the filename based on the current date and time
