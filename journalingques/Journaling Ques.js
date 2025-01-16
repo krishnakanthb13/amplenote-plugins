63fbd7b48cd20d1ba49167ffeeca0bc3d36606b0
Krishna Kanth B <75969083+krishnakanthb13@users.noreply.github.com>
2025-01-16 16:07:19 +0530

    Create Journaling Ques.js

---
+++ journalingques/Journaling Ques.js
@@ -0,0 +1,121 @@
+{
+  async appOption(app) {
+
+  // Prompt the user for input on desired actions with the selected text.
+  const result = await app.prompt("Journaling Questions to start with. Disclaimer: Please be aware that humans may review or read any shared content to ensure compliance, quality, and accuracy in accordance with Gemini's policies.", {
+    inputs: [
+      // Selection for Gemini Model Variants
+      { 
+        label: "Gemini Model variants", 
+        type: "select", 
+        options: [
+          { label: "Gemini 1.5 Pro", value: "gemini-1.5-pro" }, 
+          { label: "Gemini 2.0 Flash", value: "gemini-2.0-flash-exp" }, 
+          { label: "Gemini 1.0 Pro", value: "gemini-1.0-pro" }
+        ],
+        value: "gemini-1.5-pro"
+      },
+/*       // Options for Prompting Type
+      { 
+        label: "Prompting Type", 
+        type: "select", 
+        options: [
+          { label: "Summarize", value: "Summarize this text" },
+          { label: "Rephrase", value: "Rephrase this text" },
+          { label: "Explain/Define (Points)", value: "Explain or Define this text as points" },
+          { label: "Explain/Define (Paragraph)", value: "Explain or Define this text as paragraph" },
+          { label: "Actionable Points", value: "Convert into Actionable Points" },
+          { label: "Answer Question", value: "Answer the below question" },
+          { label: "Complete the Sentence", value: "Complete the Sentence" },
+          { label: "Rhymes With (One-word)", value: "Give 5 Rhyming Synonyms and Antonyms" },
+          { label: "Other (Fill following boxes)", value: "Customized - Use the below Details" }
+        ],
+        value: "Summarize this text"
+      }, */
+      // Additional user inputs for customization of the AI response
+	  { label: "Topic that you are tyring to Jounal About?", placeholder: "Eg: Self-Reflection, Self-Exploratory, Loneliness, Love, Hatred, Emotional State, Money, etc,.", type: "text" },
+	  { label: "Number of Question?", type: "text", value: 5 },
+    ]
+  });
+
+  // Exit if the user cancels the operation
+  if (!result) {
+    app.alert("Operation has been cancelled. Tata! Bye Bye! Cya!");
+    return;
+  }
+  
+  app.alert("Operation has started. It may take a couple of seconds for it to complete!");
+
+  // Extract user-selected inputs
+  const [modelVariant, promptDetails, numberOfQuestions] = result;
+  // console.log("result",result);  
+  const modelVariantz = modelVariant;
+  // console.log("modelVariantz",modelVariantz);
+  let finalAIResponse;
+
+  //---------------------------
+  // Load the external Google Generative AI library
+  //---------------------------
+  function _loadLibrary(url) {
+    return import(url);
+  }
+
+  _loadLibrary("https://esm.run/@google/generative-ai").then(async ({ GoogleGenerativeAI, HarmBlockThreshold, HarmCategory }) => {
+	// Safety Settings
+	const safetySettings = [
+	  {
+		category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+		threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+	  },
+	  {
+		category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+		threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+	  },
+	];
+
+    // Initialize GoogleGenerativeAI instance with API key
+    const API_KEY = app.settings["Gemini API Key"]; // Replace with your actual API key
+    const genAI = new GoogleGenerativeAI(API_KEY);
+
+    // Fetch the generative model specified by the user
+    const aiModel = genAI.getGenerativeModel({ model: `${modelVariantz}`, safetySettings });
+	// console.log("aiModel",aiModel);
+
+    // Construct the prompt to be sent to the AI model
+    const promptAI = `Give me [${numberOfQuestions || "5"}] Questions to Journal on the Topic: ${promptDetails || "Random Topic"}.\nFinal out put should be in a Markdown Formatted and the content should guide the user to navigate through the Topic mentioned.\nEg: (### Heading\n- Q1: XYZ?\n- A1:\n\n- Q2: XYZ?\n- A2:\n....`;
+	// console.log("promptAI",promptAI);
+    
+    // Generate content based on the constructed prompt
+    const aiResponse = await aiModel.generateContent(promptAI);
+	// console.log("aiResponse",aiResponse);
+    finalAIResponse = aiResponse.response.text();
+	// console.log("finalAIResponse",finalAIResponse);
+
+    // Define a unique filename for the new note, if that option is selected
+    const now = new Date();
+    const YYMMDD = now.toISOString().slice(2, 10).replace(/-/g, '');
+    const HHMMSS = now.toTimeString().slice(0, 8).replace(/:/g, '');
+    const filename = `Journaling_Questions_${YYMMDD}_${HHMMSS}`;
+	
+	// finalAIResponse += `\n### *<mark>Expand to Read more: Input Details:</mark>* <!-- {"collapsed":true} -->\n`;
+	finalAIResponse += `\n> Topic: ${promptDetails || "Random Topic"}.\n> Number of Questions: ${numberOfQuestions}.\n> When: ${filename}`;
+	// finalAIResponse += `\n---`;
+
+	// return finalAIResponse;
+	// app.insertNoteContent({ uuid: noteUUID }, finalAIResponse);
+	// console.log("Journaling Ques - Content Added.");
+
+	const noteUUIDNew = await app.createNote(`${promptDetails || "Random Topic"}`, [ "-reports/-journal-ques" ]);
+	// console.log("New note Created.");
+	await app.insertContent({ uuid: noteUUIDNew }, finalAIResponse);
+	await app.navigate(`https://www.amplenote.com/notes/${noteUUIDNew}`);
+	// console.log("Journaling Ques - Content Added.");
+	
+	app.alert(`Operation has been Completed. You can start Journaling / Answering the Questions on the mentioned topic.`);
+    
+  }).catch(error => {
+    console.error("Failed to load library or execute code:", error);
+  });
+
+  },
+}
